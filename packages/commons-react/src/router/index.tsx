5deb0cc6403b67b579223510b127a1c2c41054b1
Bane.Shi <fengxiaotx@163.com>
2024-09-23 11:31:46 +0800

    feat: 调整 route-dom export内容

--- packages/commons-react/src/router/index.tsx
+++ packages/commons-react/src/router/index.tsx
@@ -1,23 +1,8 @@
 import {trionesCreateBrowserRouter, trionesCreateHashRouter} from "./hooks";
-import {RouteItem,TrionesRouteObject} from "./RouteObject";
+import {RouteItem, TrionesRouteObject} from "./RouteObject";
 
-export type {RouteItem,TrionesRouteObject as RouteObject};
-export {
-    trionesCreateBrowserRouter as createBrowserRouter,
-    trionesCreateHashRouter as createHashRouter,
-};
-
-export {
-    Link,
-    NavLink,
-    useSearchParams,
-    useSubmit,
-    useFormAction,
-    useFetcher,
-    useFetchers,
-} from "react-router-dom"
-
-export type {
+import {
+    //--- type
     unstable_DataStrategyFunction,
     unstable_DataStrategyFunctionArgs,
     unstable_DataStrategyMatch,
@@ -28,9 +13,7 @@ export type {
     SubmitOptions,
     URLSearchParamsInit,
     V7_FormMethod,
-} from "react-router-dom"
-
-export type {
+    //---
     ActionFunction,
     ActionFunctionArgs,
     AwaitProps,
@@ -80,9 +63,18 @@ export type {
     UIMatch,
     unstable_HandlerResult,
     unstable_PatchRoutesOnNavigationFunction,
-} from "react-router-dom"
-
-export type {
+    //-- type
+    //-----------------
+    RouterProvider,
+    // ----------
+    Link,
+    NavLink,
+    useSearchParams,
+    useSubmit,
+    useFormAction,
+    useFetcher,
+    useFetchers,
+    //----
     AbortedDeferredError,
     Await,
     MemoryRouter,
@@ -129,4 +121,134 @@ export type {
     useRouteError,
     useRouteLoaderData,
     useRoutes,
+
 } from "react-router-dom"
+
+
+export type {
+    unstable_DataStrategyFunction,
+    unstable_DataStrategyFunctionArgs,
+    unstable_DataStrategyMatch,
+    FormEncType,
+    FormMethod,
+    GetScrollRestorationKeyFunction,
+    ParamKeyValuePair,
+    SubmitOptions,
+    URLSearchParamsInit,
+    V7_FormMethod,
+    //---
+    ActionFunction,
+    ActionFunctionArgs,
+    AwaitProps,
+    Blocker,
+    BlockerFunction,
+    DataRouteMatch,
+    DataRouteObject,
+    ErrorResponse,
+    Fetcher,
+    FutureConfig,
+    Hash,
+    IndexRouteObject,
+    IndexRouteProps,
+    JsonFunction,
+    LazyRouteFunction,
+    LayoutRouteProps,
+    LoaderFunction,
+    LoaderFunctionArgs,
+    Location,
+    MemoryRouterProps,
+    NavigateFunction,
+    NavigateOptions,
+    NavigateProps,
+    Navigation,
+    Navigator,
+    NonIndexRouteObject,
+    OutletProps,
+    Params,
+    ParamParseKey,
+    Path,
+    PathMatch,
+    Pathname,
+    PathParam,
+    PathPattern,
+    PathRouteProps,
+    RedirectFunction,
+    RelativeRoutingType,
+    RouteMatch,
+    RouteProps,
+    RouterProps,
+    RouterProviderProps,
+    RoutesProps,
+    Search,
+    ShouldRevalidateFunction,
+    ShouldRevalidateFunctionArgs,
+    To,
+    UIMatch,
+    unstable_HandlerResult,
+    unstable_PatchRoutesOnNavigationFunction,
+    //---
+    RouteItem, TrionesRouteObject as RouteObject
+};
+export {
+    RouterProvider,
+    // ----------
+    Link,
+    NavLink,
+    useSearchParams,
+    useSubmit,
+    useFormAction,
+    useFetcher,
+    useFetchers,
+    //----
+    AbortedDeferredError,
+    Await,
+    MemoryRouter,
+    Navigate,
+    NavigationType,
+    Outlet,
+    Route,
+    Router,
+    Routes,
+    createMemoryRouter,
+    createPath,
+    createRoutesFromChildren,
+    createRoutesFromElements,
+    defer,
+    isRouteErrorResponse,
+    generatePath,
+    json,
+    matchPath,
+    matchRoutes,
+    parsePath,
+    redirect,
+    redirectDocument,
+    replace,
+    renderMatches,
+    resolvePath,
+    useActionData,
+    useAsyncError,
+    useAsyncValue,
+    useBlocker,
+    useHref,
+    useInRouterContext,
+    useLoaderData,
+    useLocation,
+    useMatch,
+    useMatches,
+    useNavigate,
+    useNavigation,
+    useNavigationType,
+    useOutlet,
+    useOutletContext,
+    useParams,
+    useResolvedPath,
+    useRevalidator,
+    useRouteError,
+    useRouteLoaderData,
+    useRoutes,
+    //---
+    trionesCreateBrowserRouter as createBrowserRouter,
+    trionesCreateHashRouter as createHashRouter,
+};
+
+
