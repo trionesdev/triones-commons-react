22d7b030918f05085e5473469a26cf55457104e5
落雁沙 <fengxiaotx@163.com>
2025-02-15 12:06:13 +0800

    feat:init

--- examples/react-demo/src/SecurityPage.tsx
+++ examples/react-demo/src/SecurityPage.tsx
@@ -1,4 +1,6 @@
+import {useMatches} from "@trionesdev/commons-react";
 export const SecurityPage = () => {
+    console.log(useMatches())
     return <div>
         <div style={{textAlign: "center"}}>
             <h1>Security Page</h1>
